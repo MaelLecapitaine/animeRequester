7c388f45d64632419e8307bb15382943993c7b3b
Maël Lecapitaine <mael.lecapitaine@gmail.com>
2025-09-29 09:56:00 +0200

    Début de requête pour tableau genres

--- Api.js
+++ Api.js
@@ -1,14 +1,3 @@
-const data = null;
-
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
-
 let titre = null;
 let genres = null;
 let id = null;
@@ -17,6 +6,25 @@ let id = null;
 
 const dataGenre = null;
 
+const xhrGenres = new XMLHttpRequest();
+xhrGenres.withCredentials = true;
+
+xhrGenres.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhrGenres.open('GET', 'https://anime-db.p.rapidapi.com/genre');
+xhrGenres.setRequestHeader('x-rapidapi-key', '04335e7f5cmshf46b5a783eaa1dep1b6fabjsn8dcb23c9ceb0');
+xhrGenres.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
+xhrGenres.send(dataGenre);
+
+console.log(dataGenre.keys);
+
+//Recherche Par titre
+
+const data = null;
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
@@ -26,12 +34,6 @@ xhr.addEventListener('readystatechange', function () {
 	}
 });
 
-xhr.open('GET', 'https://anime-db.p.rapidapi.com/genre');
-xhr.setRequestHeader('x-rapidapi-key', '04335e7f5cmshf46b5a783eaa1dep1b6fabjsn8dcb23c9ceb0');
-xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
-xhr.send(dataGenre);
-
-//Recherche par titre
 if(titre != null && genres == null){
     xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search='+ titre +'&sortBy=ranking&sortOrder=asc');
 }
@@ -39,6 +41,7 @@ if(titre != null && genres == null){
 else if(titre == null && genres != null){
     xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&genres='+ genres +'&sortBy=ranking&sortOrder=asc');
 }
+
 xhr.setRequestHeader('x-rapidapi-key', '04335e7f5cmshf46b5a783eaa1dep1b6fabjsn8dcb23c9ceb0');
 xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
